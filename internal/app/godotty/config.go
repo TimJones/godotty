455d960a4b7d10dcf5937fc7000971198d2a9cef
Tim Jones <tim@freakinabox.com>
2019-02-04 21:53:46 +0100

    Create empty config file if it doesn't exist

--- internal/app/godotty/config.go
+++ internal/app/godotty/config.go
@@ -1,13 +1,25 @@
 package godotty
 
 import (
+	"os"
+
 	"github.com/BurntSushi/toml"
 )
 
-func LoadFromFile(file string) (*GodottyConfig, error) {
+func LoadFromFile(filepath string) (*GodottyConfig, error) {
 	var config GodottyConfig
-	if _, err := toml.DecodeFile(file, &config); err != nil {
+	if _, err := toml.DecodeFile(filepath, &config); err != nil {
 		return nil, err
 	}
 	return &config, nil
+}
+
+func (config *GodottyConfig) SaveFile(filepath string) error {
+	file, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	enc := toml.NewEncoder(file)
+	return enc.Encode(config)
 }
